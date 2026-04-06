e343feead1a32af53379333268f500428b009c47
Giorgos Saridakis <37654456+giorgossaridakis@users.noreply.github.com>
2026-04-06 14:12:50 +0300

    Add files via upload

---
+++ lock.h
@@ -0,0 +1,8 @@
+// lock_h header
+
+#ifndef lock_h__
+#define lock_h__
+
+extern int license(FILE *);
+
+#endif
---
+++ testlock.c
@@ -0,0 +1,32 @@
+// test lock
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <stdint.h>
+#include <ctype.h>
+#include <time.h>
+#include <math.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <stdbool.h>
+#include <pwd.h>
+#include <signal.h>
+#include <sys/time.h>
+#include <lock.h>
+
+#define VERSION 0.6
+
+int locker;
+
+int main(int argc, char *argv[])
+{
+
+    while ( 1 ) {
+     license( stdout );
+     sleep( 1 );
+    }
+
+
+ return EXIT_SUCCESS;
+}
